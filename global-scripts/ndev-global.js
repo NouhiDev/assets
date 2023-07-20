1218aa31c5bc95aa59907d3a97a9d750d564009a
nouhi <wolfgraphics.business@gmail.com>
2023-07-20 21:28:36 +0200

    Update ndev-global.js

--- global-scripts/ndev-global.js
+++ global-scripts/ndev-global.js
@@ -65,16 +65,3 @@ async function usageDisplay() {
     https://nouhi.dev/ndev-assets-docs/.
     `);
 }
-
-if (document.getElementById("cards")) {
-    document.getElementById("cards").onmousemove = (e) => {
-        for (const card of document.getElementsByClassName("card")) {
-            const rect = card.getBoundingClientRect(),
-                x = e.clientX - rect.left,
-                y = e.clientY - rect.top;
-    
-            card.style.setProperty("--mouse-x", `${x}px`);
-            card.style.setProperty("--mouse-y", `${y}px`);
-        }
-    };
-}
