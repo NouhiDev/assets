16f8319a046a776944d84fe8ebb7ca9156371541
nouhi <wolfgraphics.business@gmail.com>
2023-07-24 13:17:07 +0200

    Update ndev-roblox-horrorlist-api.js

--- global-scripts/ndev-roblox-horrorlist-api.js
+++ global-scripts/ndev-roblox-horrorlist-api.js
@@ -68,21 +68,12 @@ async function fetchSpreadSheetData() {
     var genreHTMLText = genreHTML(genreArray);
     var gameURL = "https://www.roblox.com/games/" + gameDataFromAPI[i].rootPlaceId;
 
-    var row = ` <tr class="hover-reveal" data-tooltip="${toolTipContent(
-      data.spreadSheetData,
-      gameDataFromAPI,
-      gameIconDataFromAPI,
-      i,
-      genreHTMLText
-    )}">
+    var row = ` <tr class="hover-reveal">
           <td data-th="Placement">${i + 1}.</td>
           <td data="Icon"><img class="game-icon" src="${gameIconDataFromAPI[i].imageUrl}"></td>
-          <td data-th="Title" class="game-title"><a href="#" class="game-href" onlick="loadGame(
+          <td data-th="Title" class="game-title"><a href="#" class="game-href" onclick="loadGame(
             ${i + 1}, 
-            ${data.spreadSheetData},
-            ${gameDataFromAPI[i]},
-            ${gameIconDataFromAPI[i]}
-            )">${gameDataFromAPI[i].name}</a></td>
+            ${gameUIDS[i]})">${gameDataFromAPI[i].name}</a></td>
           <td data-th="Creator" class="align-left">${JSON.parse(
       JSON.stringify(gameDataFromAPI[i].creator)
     ).name}</td>
@@ -149,129 +140,6 @@ function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-
-function toolTipContent(spreadSheetData, apiGameData, apiGameIconData, i, genreHTMLText) {
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
-  var desc = "";
-  if (apiGameData[i].description === null) desc = "This game does not have a description.";
-  else desc = apiGameData[i].description.replaceAll('"', "")
-
-  return `
-      x01${apiGameData[i].name}
-      x02${JSON.parse(JSON.stringify(apiGameData[i].creator))["name"]}
-      x03${desc}
-      x04${genreHTMLText}
-      x05${spreadSheetData[i].Date}
-      xEND
-  
-      xR01${spreadSheetData[i].Scariness}
-      xR02${spreadSheetData[i].SoundDesign}
-      xR03${spreadSheetData[i].Story}
-      xR04${spreadSheetData[i].Visuals}
-      xR05${spreadSheetData[i].Ambience}
-      xR06${spreadSheetData[i].Gameplay}
-      xR07${spreadSheetData[i].Creativity}
-      xR08${spreadSheetData[i].Enjoyment}
-      xR09${spreadSheetData[i].ProductionQuality}
-      xR10${spreadSheetData[i].Technical}
-      xREND
-  
-      xSD01${formatter.format(apiGameData[i].playing)}
-      xSD02${formatter.format(apiGameData[i].favoritedCount)}
-      xSD03${formatter.format(apiGameData[i].visits)}
-      xSD04${formatter.format(apiGameData[i].maxPlayers)}
-      xSD05${(apiGameData[i].created.substring(0, 10))}
-      xSD06${(apiGameData[i].updated.substring(0, 10))}
-      xSDEND
-      `;
-}
-
-
-/* Tooltip */
-let setUpTooltip = function () {
-  let tooltip = "",
-    toolTipDiv = document.querySelector(".tooltip-class"),
-    toolTipElements = Array.from(document.querySelectorAll(".hover-reveal"));
-
-  // Game General Info Display  
-  let gameTitle = document.getElementById("game_tooltip_title");
-  let gameCreator = document.getElementById("game_tooltip_creator");
-  let gameDescription = document.getElementById("game_tooltip_description");
-  let lastUpdate = document.getElementById("game_tooltip_lastUpdate");
-
-  // Game Ranking Display
-  let note = document.getElementById("note");
-  let scary = document.getElementById("game_rating_scariness");
-  let soundDesign = document.getElementById("game_rating_soundDesign");
-  let story = document.getElementById("game_rating_story");
-  let visuals = document.getElementById("game_rating_visuals");
-  let ambience = document.getElementById("game_rating_ambience");
-  let gameplay = document.getElementById("game_rating_gameplay");
-  let creativity = document.getElementById("game_rating_creativity");
-  let enjoyment = document.getElementById("game_rating_enjoyment");
-  let productionQuality = document.getElementById("game_rating_productionQuality");
-  let technical = document.getElementById("game_rating_technical");
-  let players = document.getElementById("players");
-  let genre = document.getElementById("genre");
-
-  // Game Stats Display
-  let active = document.getElementById("game_stats_container_active");
-  let favorites = document.getElementById("game_stats_container_favorites");
-  let visits = document.getElementById("game_stats_container_visits");
-  let max_players = document.getElementById("game_stats_container_maxPlayers");
-  let created = document.getElementById("game_stats_container_created");
-  let updated = document.getElementById("game_stats_container_updated");
-
-  let displayTooltip = function (e, obj) {
-    tooltip = obj.dataset.tooltip;
-    toolTipDiv.style.top = e.pageY + ($(window).scrollTop() * -1) + "px";
-    toolTipDiv.style.left = e.pageX + "px";
-    toolTipDiv.style.opacity = .95;
-
-    // Game General Information  
-    gameTitle.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("x01") + 3, obj.dataset.tooltip.indexOf("x02"));
-    gameCreator.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("x02") + 3, obj.dataset.tooltip.indexOf("x03"));
-    gameDescription.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("x03") + 3, obj.dataset.tooltip.indexOf("x04"));
-    genre.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("x04") + 3, obj.dataset.tooltip.indexOf("x05"));
-    lastUpdate.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("x05") + 3, obj.dataset.tooltip.indexOf("xEND"));
-
-    // Game Rating Details
-    scary.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR01") + 4, obj.dataset.tooltip.indexOf("xR02"));
-    soundDesign.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR02") + 4, obj.dataset.tooltip.indexOf("xR03"));
-    story.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR03") + 4, obj.dataset.tooltip.indexOf("xR04"));
-    visuals.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR04") + 4, obj.dataset.tooltip.indexOf("xR05"));
-    ambience.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR05") + 4, obj.dataset.tooltip.indexOf("xR06"));
-    gameplay.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR06") + 4, obj.dataset.tooltip.indexOf("xR07"));
-    creativity.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR07") + 4, obj.dataset.tooltip.indexOf("xR08"));
-    enjoyment.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR08") + 4, obj.dataset.tooltip.indexOf("xR09"));
-    productionQuality.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR09") + 4, obj.dataset.tooltip.indexOf("xR10"));
-    technical.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xR10") + 4, obj.dataset.tooltip.indexOf("xREND"));
-
-    // Game Stats Display Text Update
-    active.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD01") + 5, obj.dataset.tooltip.indexOf("xSD02"));
-    favorites.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD02") + 5, obj.dataset.tooltip.indexOf("xSD03"));
-    visits.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD03") + 5, obj.dataset.tooltip.indexOf("xSD04"));
-    max_players.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD04") + 5, obj.dataset.tooltip.indexOf("xSD05"));
-    created.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD05") + 5, obj.dataset.tooltip.indexOf("xSD06"));
-    updated.innerHTML = obj.dataset.tooltip.substring(obj.dataset.tooltip.indexOf("xSD06") + 5, obj.dataset.tooltip.indexOf("xSDEND"));
-  };
-
-  toolTipElements.forEach(function (elem) {
-    elem.addEventListener("mouseenter", function (e) {
-      displayTooltip(e, this);
-    });
-    elem.addEventListener("mouseleave", function (e) {
-      toolTipDiv.style.top = "9999px";
-      toolTipDiv.style.left = "9999px";
-      toolTipDiv.style.opacity = 0;
-    });
-  });
-};
-
-window.addEventListener("click", () => {
-  setUpTooltip();
-});
-
 function genreHTML(genreArray) {
   var genreHTMLString = "";
 
@@ -332,10 +200,8 @@ function discordButton() {
   window.open("https://discord.gg/Zbxst3g4ts", "_blank");
 }
 
-function loadGame(number, spreadSheetData, gameData, gameIconData) {
+function loadGame(number, UID) {
   localStorage.setItem('number', number);
-  localStorage.setItem('spreadSheetData', spreadSheetData);
-  localStorage.setItem('gameData', gameData);
-  localStorage.setItem('gameIconData', gameIconData);
+  localStorage.setItem('UID', UID);
   window.location.href = 'game.html';
 }
