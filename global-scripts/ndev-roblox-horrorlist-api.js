a5eb2cc6d244612f5bc6bde7e71a9737d71651e6
nouhi <wolfgraphics.business@gmail.com>
2023-08-01 15:24:28 +0200

    Update ndev-roblox-horrorlist-api.js

--- global-scripts/ndev-roblox-horrorlist-api.js
+++ global-scripts/ndev-roblox-horrorlist-api.js
@@ -48,24 +48,18 @@ async function fetchData() {
         chunks.push(gameUIDS.slice(i, i + maxUIDChunkSize));
     }
 
-    console.time("Fetch Game Info");
     const fetchGameDataPromises = chunks.map((chunk) =>
         fetch(`${API_BASE_URL}/game-info/${chunk.join(",")}`).then((response) => response.json())
     );
-    console.timeEnd("Fetch Game Info");
 
-    console.time("Fetch Game Icon")
     const fetchIconDataPromises = chunks.map((chunk) =>
         fetch(`${API_BASE_URL}/game-icon/${chunk.join(",")}`).then((response) => response.json())
     );
-    console.timeEnd("Fetch Game Icon");
 
     elem.style.width = "50%";
 
-    console.time("Fetch Game Info & Icon")
     const gameDataResponses = await Promise.all(fetchGameDataPromises);
     const iconDataResponses = await Promise.all(fetchIconDataPromises);
-    console.timeEnd("Fetch Game Info & Icon")
 
     console.log(fetchGameDataPromises);
     console.log(gameDataResponses);
